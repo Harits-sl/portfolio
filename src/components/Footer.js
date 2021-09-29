655aedfff7816e9c0da2ffb5a9574834d225e667
Harits Salsabil <harits.salsabil@gmail.com>
2021-09-29 09:15:17 +0700

    remove contact

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,8 +6,8 @@ const Footer = () => {
     <footer className="footer px-3 py-2">
       <Container fluid>
         <h1 className="fs-4 fw-normal">Contact me</h1>
-        <p className="mb-0 text-muted">(+62) 81316945162</p>
-        <p className="mb-0 text-muted">harits.salsabil@gmail.com</p>
+        {/* <p className="mb-0 text-muted">(+62) 81316945162</p> */}
+        {/* <p className="mb-0 text-muted">harits.salsabil@gmail.com</p> */}
       </Container>
     </footer>
   );
